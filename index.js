27c33c8eb3ae6e1c8621086698944e1d2fcd849a
Grace Wangui <wanguigrace280@gmail.com>
2022-08-17 09:38:00 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = () => 2000/100
+const square = x => x * x
+
+const add  = (parameter1, parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2;
+  }
